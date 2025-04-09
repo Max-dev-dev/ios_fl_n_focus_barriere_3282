be1e97504c1a0d8c66502d78b13811e641e7e0fa
Max-dev-dev <n.ilkiv@upstars.com>
2025-04-09 17:33:49 +0300

    update app version

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,19 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        return Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'assets/images/main_background.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            child ?? const SizedBox.shrink(),
+          ],
+        );
+      },
       theme: ThemeData(
         textTheme: const TextTheme(
           bodyLarge: TextStyle(color: Colors.white),
@@ -30,8 +43,6 @@ class MainApp extends StatelessWidget {
           labelMedium: TextStyle(color: Colors.white),
           labelSmall: TextStyle(color: Colors.white),
         ),
-        appBarTheme: const AppBarTheme(backgroundColor: Color.fromARGB(255, 85, 83, 83)),
-        scaffoldBackgroundColor: Color.fromARGB(255, 85, 83, 83),
       ),
       home: SplashScreen(),
     );
